ad0e5bd34db4ade4706c56da2b0ed0d87a8040f4
peteshobby <pjbdeveloper@gmail.com>
2025-10-15 17:49:44 -0400

    Prevented ClearScreen from chaning the border

--- aqplus/aqplus_graphics.c
+++ aqplus/aqplus_graphics.c
@@ -6,18 +6,36 @@ static uint8_t  *colorRam    = (uint8_t *)  COLRRAM;
 
 #define CLRSCRN 	0x1E45
 #define FILLSCRN	0x1e59
+#define SCRSIZE		1000
 
-#define ARG_COLOR	0
-
+// The rom FILLSCRN routine is hard coded to fill 1024 bytes. That does not 
+// matter on an original Aquarius but on the Aquarius+ the border control
+// attribute can be remapped to 1023 to allow every screen positon to have its 
+// own character and color without affecting the boarder.
+// As the screen is only 40x25 = 1000 positions we can clear it without 
+// affecting the border by only writing 1000 bytes.
 void ClearScreen(uint8_t color) {
+	(void) color; // asm takes the parameter directly from the A register.
 __asm
-	push	af
+	ld		hl, #COLRRAM 
+	ld 		b, a
+	call 	Fill
 	ld 		b, #SPACE_CHAR
 	ld 		hl, #CHARRAM
-	call 	FILLSCRN
-	pop		af
-	ld 		b, a
-	call 	FILLSCRN
+	call 	Fill 
+	jp		FillEnd
+
+Fill:
+	ld		de, #SCRSIZE
+FillLoop:
+	ld		(hl), b
+	inc		hl
+	dec		de
+	ld		a, d
+	or		e
+	jr		nz, FillLoop
+	ret
+FillEnd:
 __endasm;
 }
 
@@ -32,5 +50,5 @@ void SetBorder(uint8_t color) {
 // It also sets the border to the same color.
 void ClearTextScreen(uint8_t backgroundColor) {
 	ClearScreen(COLOR(0, backgroundColor));
-    SetBorder(backgroundColor);
+
 }
