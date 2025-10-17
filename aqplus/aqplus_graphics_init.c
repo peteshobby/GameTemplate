da5068ec367ca7111ec351c769c4c9a71f2cdc20
peteshobby <pjbdeveloper@gmail.com>
2025-10-17 19:44:38 -0400

    Moved Aqplus graphics initialization to its own file.

--- aqplus/aqplus_graphics_init.c
+++ aqplus/aqplus_graphics_init.c
@@ -15,27 +15,22 @@
  */
 
 /**
- * @file  aqplus_initialization.c
- * @brief Aquarius+ one time initialization functions.
+ * @file  aqplus_graphics_init.c
+ * @brief Aquarius+ one time graphics initialization functions.
  */
 
-#include "initialization.h"
 #include "graphics.h"
-#include "aqplus_graphics.h"
-#include "system_constants.h"
+#include "constants.h"
+
 #include <aqplus.h>
 
-#define TURBO_MODE		0b00000100
-#define UNLIMITED_MODE	0b00001000
-
-
-void EnableTurboMode(void) {
-    IO_SYSCTRL |= TURBO_MODE;
-}
-
-void InitializeSystem() {
-	EnableTurboMode();
-	InitializeGraphics();
-	ClearTextScreen(0);
-	SetBorder(0);
+// This function:
+// 1. sets the layer priorities
+// 2. remaps the border character
+void InitializeGraphics(void) {
+	IO_VCTRL =  VCTRL_MODE_TILE | 
+				VCTRL_TEXT_PRIO |
+				VCTRL_SPR_EN | 
+				VCTRL_TEXT_EN | 
+				VCTRL_REMAP_BORDER_CH;
 }
